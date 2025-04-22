30370391527daa60e3db2b003254a40f07944980
Shraddha-013 <sjshraddha13@gmail.com>
2025-04-22 12:45:11 +0530

    updated

--- sample.java
+++ sample.java
@@ -3,6 +3,7 @@ package sample;
 public class sample {
     public static void main(String[] args) {
         System.out.println("hello Main");
+        System.out.println("hello Master");
         
     }
 }
